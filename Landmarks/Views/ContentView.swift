cbff74a98e8fd196e974ed69de69bfb2e07d5ebe
Jared Tamulynas <jwtamuly@ncsu.edu>
2021-11-18 13:26:14 -0500

    Adding Part 1 and 2

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Jared Tamulynas on 11/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
